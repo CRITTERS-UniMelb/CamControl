031fd54ca97f16f29586c90664f90f1f5819ce50
Saoirse Kelleher <saoirse.kelleher@unimelb.edu.au>
2026-08-11 13:20:23 +1000

    remove auto-exposure from threadcamera

--- thread_camera.py
+++ thread_camera.py
@@ -18,7 +18,6 @@ class CameraThread(QThread):
         self.cameraName = None
         self.hcam = None
         self.buf = None
-        self.autoExposure = True
         self.running = False
 
     def connectCamera(self):
@@ -30,20 +29,6 @@ class CameraThread(QThread):
         self.height = 500
         self.imageMinimizedWidth = 500
         self.imageMinimizedHeight = 500
-        
-
-    @staticmethod
-    def cameraCallback(nEvent, ctx):
-        ctx.CameraCallback(nEvent)
-
-
-    def CameraCallback(self, nEvent):
-        if nEvent == uvcham.UVCHAM_EVENT_IMAGE:
-            img = QImage(self.buf, self.width, self.height, (self.width * 24 + 31) // 32 * 4, QImage.Format_BGR888)
-            self.cameraImage.emit(img)
-        else:
-            pass
-            # print('event callback: {}'.format(nEvent))
 
 
     def run(self):
@@ -65,21 +50,6 @@ class CameraThread(QThread):
                        image.shape[0],
                        QImage.Format.Format_RGB888)
         return image
-
-    def changeAutoExposure(self, state):
-        if self.hcam is not None:
-            if state is True:
-                #self.hcam.put(uvcham.UVCHAM_AEXPO, 1)
-                print("ELLY:    Camera Auto Exposure Enabled")
-                self.autoExposure = True
-            elif state is False:
-                #self.hcam.put(uvcham.UVCHAM_AEXPO, 0)
-                print("ELLY:    Camera Auto Exposure Disabled")
-                self.autoExposure = False
-    
-    def changeExposureTime(self, time):
-        if (self.hcam is not None) and (self.autoExposure is False):
-                self.hcam.put(uvcham.UVCHAM_EXPOTIME, time)
 
 
     def stop(self):
